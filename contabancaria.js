03c8c925c8ad2c87c64e35742704fe71ceda4055
jujulilia <jujuscosta07@gmail.com>
2023-08-24 14:05:11 -0300

    Exercicio 2 - Atualização conta bancária

--- contabancaria.js
+++ contabancaria.js
@@ -15,20 +15,32 @@ class contaBancaria {
         return this.nomeDoTitular + " possui R$" + this.saldoAtual + " em sua conta bancária atualmente.";
     }
     realizarSaque(saque) {
-        let msgSacarSaldo = "saldo aprovado R$";
+        let msgSacarSaldo = "saque aprovado. ";
         if (saque > this.saldoAtual) {
             msgSacarSaldo = "saque negado.";
             return msgSacarSaldo;
         }
         else {
             this.saldoAtual = this.saldoAtual - saque;
-            return msgSacarSaldo + saque + "O saldo atual é R$" + this.saldoAtual;
+            return msgSacarSaldo + saque + " Seu saldo atual é R$" + this.saldoAtual;
         }
     }
 }
 let minhaConta = new contaBancaria("João", 1000);
+let minhaContaDois = new contaBancaria("Julia", 10000);
+let minhaContaTres = new contaBancaria("Ana", 8000);
 console.log(minhaConta.saldoInicial());
 console.log(minhaConta.depositoDaConta(500));
 console.log(minhaConta.saldoDaConta());
 console.log(minhaConta.realizarSaque(200));
 console.log(minhaConta.realizarSaque(1500));
+console.log(minhaContaDois.saldoInicial());
+console.log(minhaContaDois.depositoDaConta(500));
+console.log(minhaContaDois.saldoDaConta());
+console.log(minhaContaDois.realizarSaque(200));
+console.log(minhaContaDois.realizarSaque(1500));
+console.log(minhaContaTres.saldoInicial());
+console.log(minhaContaTres.depositoDaConta(500));
+console.log(minhaContaTres.saldoDaConta());
+console.log(minhaContaTres.realizarSaque(200));
+console.log(minhaContaTres.realizarSaque(1500));
--- contabancaria.ts
+++ contabancaria.ts
@@ -22,26 +22,47 @@ class contaBancaria{
     }
 
     realizarSaque(saque: number){
-        let msgSacarSaldo = "saldo aprovado R$";
+        let msgSacarSaldo = "saque aprovado. ";
         if (saque > this.saldoAtual){
             msgSacarSaldo = "saque negado.";
             return msgSacarSaldo;
         } else{
             this.saldoAtual = this.saldoAtual - saque;
-            return msgSacarSaldo + saque + "O saldo atual é R$" + this.saldoAtual;
+            return msgSacarSaldo + saque + " Seu saldo atual é R$" + this.saldoAtual;
         }
     }
-   
-
 }
 
 
 let minhaConta = new contaBancaria("João", 1000);
+let minhaContaDois = new contaBancaria("Julia", 10000);
+let minhaContaTres = new contaBancaria("Ana", 8000);
+
+
 console.log(minhaConta.saldoInicial());
 console.log(minhaConta.depositoDaConta(500));
 console.log(minhaConta.saldoDaConta());
 console.log(minhaConta.realizarSaque(200));
 console.log(minhaConta.realizarSaque(1500));
+
+
+
+console.log(minhaContaDois.saldoInicial());
+console.log(minhaContaDois.depositoDaConta(500));
+console.log(minhaContaDois.saldoDaConta());
+console.log(minhaContaDois.realizarSaque(200));
+console.log(minhaContaDois.realizarSaque(1500));
+
+
+console.log(minhaContaTres.saldoInicial());
+console.log(minhaContaTres.depositoDaConta(500));
+console.log(minhaContaTres.saldoDaConta());
+console.log(minhaContaTres.realizarSaque(200));
+console.log(minhaContaTres.realizarSaque(1500));
+
+
+
+
 
 
 
