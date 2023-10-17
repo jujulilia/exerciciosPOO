a192edc8eab80694bc7065d64c280c435408af45
jujulilia <jujuscosta07@gmail.com>
2023-10-17 13:25:52 -0300

    exercicios com enum

---
+++ Pedidos.ts
@@ -0,0 +1,62 @@
+enum StatusPedido{
+    AguardandoPreparo = "Aguardando Preparo",
+    EmPreparo = "Em Preparo",
+    SaiuParaEntrega = "Saiu Para Entrega",
+    Entregue = "Entregue"
+}
+
+
+class ProdutoPedido{
+    nome: string;
+    valor: number;
+
+    constructor(nome: string, valor: number){
+        this.nome = nome;
+        this.valor = valor;
+    }
+}
+
+class Pedido{
+   // private produtos={nome: string, valor: number}[]; 
+   private produtos:ProdutoPedido[];
+    private status: StatusPedido;
+    
+    constructor(){
+        this.produtos =[];
+        this.status = StatusPedido.AguardandoPreparo;
+    }
+   
+
+    adicionarProduto(produto: ProdutoPedido){
+        this.produtos.push(produto);
+    }
+
+    atualizarStatus(status: StatusPedido){
+        this.status = status;
+    }
+
+    exibirStatus(){
+        return this.status;
+    }
+
+    exibirProdutosDoPedido(){
+        for (let i = 0; i< this.produtos.length; i++){
+            console.log(this.produtos[i].nome + "no valor" + this.produtos[i].valor);
+        }
+    }
+}
+
+const pedido1 = new ProdutoPedido("Fone", 99.90);
+const pedido2 = new ProdutoPedido("Celular", 3529.00);
+const pedido3 = new ProdutoPedido("Capinha", 30.00);
+
+const novoPedido = new Pedido();
+console.log("Statudo atual do pedido: " + novoPedido.exibirStatus());
+novoPedido.atualizarStatus(StatusPedido.AguardandoPreparo);
+novoPedido.adicionarProduto(pedido1);
+novoPedido.adicionarProduto(pedido2);
+novoPedido.adicionarProduto(pedido3);
+console.log("Statudo atual do Pedido: " + novoPedido.exibirStatus());
+novoPedido.atualizarStatus(StatusPedido.EmPreparo);
+console.log(novoPedido.exibirStatus());
+novoPedido.exibirProdutosDoPedido();
